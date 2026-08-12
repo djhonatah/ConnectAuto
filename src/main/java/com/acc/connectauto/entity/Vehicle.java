2ce19589225261b115acecde42a5039c6026c658
Djhonatah Wesley <djhonatahwesley2402@gmail.com>
2026-08-12 15:39:33 -0300

    feat: add Vehicle entity with JPA annotations and Lombok configuration

--- src/main/java/com/acc/connectauto/entity/Vehicle.java
+++ src/main/java/com/acc/connectauto/entity/Vehicle.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,6 +18,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "vehicle")
 @Getter
 @Setter
 @NoArgsConstructor
