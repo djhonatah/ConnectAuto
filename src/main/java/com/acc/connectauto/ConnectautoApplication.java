120eef2f086189b402a5010101e1e9bd9c6d76b1
Djhonatah Wesley <djhonatahwesley2402@gmail.com>
2026-08-12 10:15:51 -0300

    Initial commit

---
+++ src/main/java/com/acc/connectauto/ConnectautoApplication.java
@@ -0,0 +1,13 @@
+package com.acc.connectauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectautoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectautoApplication.class, args);
+	}
+
+}
